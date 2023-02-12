2d4c5474cdc974c3f39084408b1a14add8bb1704
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-02-12 18:41:31 -0500

    Fundamentos en flask

--- app/app.py
+++ app/app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
+@app.route('/')
+def index():
+    cursos = ['PHP', 'Python', 'Java', 'Kotlin', 'Dart', 'JavaScript']
+    data={
+        'titulo': 'Index123',
+        'bienvenida': 'Saludos',
+        'cursos': cursos,
+        'numero_cursos': len(cursos)
+    }
+    return render_template('index.html', data=data)
+
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre, edad):
+    data={
+        'titulo': 'Contacto',
+        'nombre': nombre,
+        'edad': edad
+    }
+    return render_template('contacto.html', data=data)
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return 'ok'
+
+
 if __name__ == '__main__':
-    app.run()
+    app.add_url_rule('/query_string', view_func=query_string)
+    app.run(debug=True, port=5000)
 
